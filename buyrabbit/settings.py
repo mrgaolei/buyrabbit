41bc56397d8404c056ccf7635de2debb4bea8e56
gaolei <guafeng@fmit.cn>
2018-10-24 11:04:18 +0800

    adjust db conf

--- buyrabbit/settings.py
+++ buyrabbit/settings.py
@@ -77,10 +77,7 @@ WSGI_APPLICATION = 'buyrabbit.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config('BR_DATABASE', cast=db_url),
 }
 
 
