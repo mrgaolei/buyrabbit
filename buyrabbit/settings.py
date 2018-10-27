99e1253693298658941fa4f893ca5185584a74e9
gaolei <guafeng@fmit.cn>
2018-10-27 13:46:07 +0800

    添加STATIC_ROOT

--- buyrabbit/settings.py
+++ buyrabbit/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR + '/static'
 
 QINIU_ACCESS_KEY = config('QINIU_ACCESS_KEY')
 QINIU_SECRET_KEY = config('QINIU_SECRET_KEY')
